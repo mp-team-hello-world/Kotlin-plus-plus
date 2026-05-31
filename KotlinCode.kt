002313ef1fa32f567bacb5e44afadc4d4a7da003
Semenikhina Anastasia <semenikhinaAA09@yandex.ru>
2026-05-31 14:32:32 +0300

    updated KotlinCode for presentation. all functions will be translated

--- KotlinCode.kt
+++ KotlinCode.kt
@@ -1,21 +1,27 @@
-fun testAllFeatures(limit: Int): Int {
-    var count = 0
-    
+fun main() {
+    val score = processData(20)
+}
+
+fun processData(limit: Int): Int {
+    var result = 0
+
     for (i in 1..limit) {
-        if (i > 10) {
+        if (i > 10 && limit > 1000) {
             break
         } else {
-            count = count + i
+            result = result + i * i
         }
     }
-    while (count < 100) {
+
+    while (result < 5000) {
         try {
-            count = count + 10
+            result *= 2
         } catch (e: Exception) {
             return -1
         } finally {
-            count = count + 1
+            result += 1
         }
     }
-    return count
+
+    return result
 }
--- TestTranslator/Unsupported.cs
+++ TestTranslator/Unsupported.cs
@@ -2,17 +2,9 @@
 using NUnit.Framework;
 public class UnsupportedCodeTests
 {
+
     [Test]
     public void Unsupported_Function_Call_1()
-    {
-        string input = "print(123)";
-        string result = Translator.Translate(input);
-
-        Assert.That(result, Does.Contain("/* Unimplemented function call: print(123) */"));
-    }
-
-    [Test]
-    public void Unsupported_Function_Call_2()
     {
         string input = "val x = 5\nval y = 6\nval z = x & y\nprintln(123)";
         string result = Translator.Translate(input);
@@ -20,11 +12,10 @@ public class UnsupportedCodeTests
         Assert.That(result, Does.Contain("auto x = 5;"));
         Assert.That(result, Does.Contain("auto y = 6;"));
         Assert.That(result, Does.Contain("/* Unsupported statement construction: val z = x & y */"));
-        Assert.That(result, Does.Contain("/* Unimplemented function call: println(123) */"));
     }
 
     [Test]
-    public void Unsupported_Code_2()
+    public void Unsupported_Code_1()
     {
         string input = "while (i in 1..x) {val y = i}";
         string result = Translator.Translate(input);
@@ -33,7 +24,7 @@ public class UnsupportedCodeTests
     }
 
     [Test]
-    public void Unsupported_Code_3()
+    public void Unsupported_Code_2()
     {
         string input = "for (i in 10 downTo 1 step 2) { i = i - 1 }";
         string result = Translator.Translate(input);
